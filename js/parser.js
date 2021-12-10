72b1db42c74a46b902bd64bdbde67e7745fd1030
monsterkodi <monsterkodi@gmx.net>
2021-12-10 03:58:42 +0100

    misc

--- js/parser.js
+++ js/parser.js
@@ -1,4 +1,4 @@
-// monsterkodi/kode 0.77.0
+// monsterkodi/kode 0.78.0
 
 var _k_ = {list: function (l) {return (l != null ? typeof l.length === 'number' ? l : [] : [])}, length: function (l) {return (l != null ? typeof l.length === 'number' ? l.length : 0 : 0)}, in: function (a,l) {return [].indexOf.call(l,a) >= 0}, extend: function (c,p) {for (var k in p) { if (Object.hasOwn(p, k)) c[k] = p[k] } function ctor() { this.constructor = c; } ctor.prototype = p.prototype; c.prototype = new ctor(); c.__super__ = p.prototype; return c;}}
 
@@ -603,10 +603,6 @@ Parser = (function ()
         k = {type:'key'}
         if (key.type)
         {
-            if (!(_k_.in(key.type,['keyword','op','punct','var','this','num','single','double','triple','bool'])))
-            {
-                console.log('what could that be?',key)
-            }
             k.text = key.text
             k.line = key.line
             k.col = key.col
